bae66486505d86ff5068f5efed14eec671cb89de
josuerojmed <135179823+josuerojmed@users.noreply.github.com>
2024-08-25 12:39:33 -0600

    Update Encriptador.js

--- js/Encriptador.js
+++ js/Encriptador.js
@@ -1,43 +1,46 @@
-//-------Boton de Encriptar-------//
-btn-encriptar.addEventListener("click", e=>{
+// -------Selección de Elementos-------
+const btnEncriptar = document.querySelector(".btn-encriptar");
+const txtEncriptar = document.querySelector(".encriptar");
+const aviso = document.querySelector(".texto-aviso");
+const respuesta = document.querySelector(".evaluar");
+const contenido = document.querySelector(".tarjeta-contenedor");
+const btnCopiar = document.querySelector(".btn-copiar");
+const btnDesencriptar = document.querySelector(".btn-desencriptar");
+
+// -------Botón de Encriptar-------
+btnEncriptar.addEventListener("click", e => {
     e.preventDefault();
-    let texto = document.querySelector(".encriptar").value;
-    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
-    
-    if(texto == ""){
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    if (texto === "") {
         aviso.style.background = "#0A3871";
         aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
-        aviso.textContent = "El campo de texto no debe estar vacio";
+        aviso.textContent = "El campo de texto no debe estar vacío";
         
-        setTimeout(()=>{
+        setTimeout(() => {
             aviso.removeAttribute("style");
-        },1500);
-    }
-
-    else if(texto !== txt){
+        }, 1500);
+    } else if (texto !== txt) {
         aviso.style.background = "#0A3871";
         aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
         aviso.textContent = "No debe tener acentos y caracteres especiales";
         
-        setTimeout(()=>{
+        setTimeout(() => {
             aviso.removeAttribute("style");
-        },1500);
-    }
-
-    else if(texto !== texto.toLowerCase()){
+        }, 1500);
+    } else if (texto !== texto.toLowerCase()) {
         aviso.style.background = "#0A3871";
         aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
         aviso.textContent = "El texto debe ser todo en minúscula";
         
-        setTimeout(()=>{
+        setTimeout(() => {
             aviso.removeAttribute("style");
-        },1500);
-    }
-
-    else{
+        }, 1500);
+    } else {
         texto = texto.replace(/e/mg, "enter");
         texto = texto.replace(/i/mg, "imes");
         texto = texto.replace(/a/mg, "ai");
@@ -45,51 +48,45 @@ btn-encriptar.addEventListener("click", e=>{
         texto = texto.replace(/u/mg, "ufat");
 
         respuesta.innerHTML = texto;
-        btnCopiar.style.visibility = "inherit";
+        btnCopiar.style.visibility = "visible";
         contenido.remove(); 
     }
 });
 
-//-------Boton de Desencriptar-------//
-btnDesencriptar.addEventListener("click", e=>{
+// -------Botón de Desencriptar-------
+btnDesencriptar.addEventListener("click", e => {
     e.preventDefault();
     let texto = txtEncriptar.value;
-    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
-    
-    if(texto == ""){
+    let txt = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+
+    if (texto === "") {
         aviso.style.background = "#0A3871";
         aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
-        aviso.textContent = "El campo de texto no debe estar vacio";
+        aviso.textContent = "El campo de texto no debe estar vacío";
         
-        setTimeout(()=>{
+        setTimeout(() => {
             aviso.removeAttribute("style");
-        },1500);
-    }
-
-    else if(texto !== txt){
+        }, 1500);
+    } else if (texto !== txt) {
         aviso.style.background = "#0A3871";
         aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
         aviso.textContent = "No debe tener acentos y caracteres especiales";
         
-        setTimeout(()=>{
+        setTimeout(() => {
             aviso.removeAttribute("style");
-        },1500);
-    }
-
-    else if(texto !== texto.toLowerCase()){
+        }, 1500);
+    } else if (texto !== texto.toLowerCase()) {
         aviso.style.background = "#0A3871";
         aviso.style.color = "#FFFF";
         aviso.style.fontWeight = "800";
         aviso.textContent = "El texto debe ser todo en minúscula";
         
-        setTimeout(()=>{
+        setTimeout(() => {
             aviso.removeAttribute("style");
-        },1500);
-    }
-
-    else{
+        }, 1500);
+    } else {
         texto = texto.replace(/enter/mg, "e");
         texto = texto.replace(/imes/mg, "i");
         texto = texto.replace(/ai/mg, "a");
@@ -97,15 +94,14 @@ btnDesencriptar.addEventListener("click", e=>{
         texto = texto.replace(/ufat/mg, "u");
 
         respuesta.innerHTML = texto;
-        btnCopiar.style.visibility = "inherit";
+        btnCopiar.style.visibility = "visible";
         contenido.remove(); 
     }
 });
 
-//-------Boton de Copiar-------//
-btnCopiar.addEventListener("click", e=>{
+// -------Botón de Copiar-------
+btnCopiar.addEventListener("click", e => {
     e.preventDefault();
-    let copiar = respuesta;
-    copiar.select();
-    document.execCommand("copy"); 
+    respuesta.select();
+    document.execCommand("copy");
 });
