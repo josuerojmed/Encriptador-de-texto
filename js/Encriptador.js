3f793af462d424b7f347ecccdc021d1b1f9f7aae
josuerojmed <135179823+josuerojmed@users.noreply.github.com>
2024-08-25 12:24:13 -0600

    Update Encriptador.js

--- js/Encriptador.js
+++ js/Encriptador.js
@@ -1,7 +1,7 @@
 //-------Boton de Encriptar-------//
 btn-encriptar.addEventListener("click", e=>{
     e.preventDefault();
-    let texto = txtEncriptar.value;
+    let texto = document.querySelector(.encriptar").value;
     let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
     
     if(texto == ""){
